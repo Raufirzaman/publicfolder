0a6bb73de65ea26151b8dcdf29c15198cd48b1a3
Raufir Zaman Choudhury <97308512+Raufirzaman@users.noreply.github.com>
2025-03-13 02:13:00 +0600

    Create challenger3.cpp

---
+++ challenger3.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+string turnCalc(string num){
+reverse(num.begin(),num.end());
+string msg="";
+for(auto i:num){
+    if(i=='1')msg+="I";
+    else if(i=='2')msg+="Z";
+    else if(i=='3')msg+="E";
+    else if(i=='4')msg+="H";
+    else if(i=='5')msg+="S";
+    else if(i=='6')msg+="G";
+    else if(i=='7')msg+="L";
+    else if(i=='8')msg+="B";
+    else if(i=='9')msg+="-";
+    else if(i=='0')msg+="O";
+}
+return msg;
+}
+int main()
+{
+ int n;
+    cin>>n;
+    cout<<turnCalc(to_string(n));  
+}
